c9c1708a18cacd3b56821cca381dfa12a5e365c7
Blendi Miftari <blendi4miftari@gmail.com>
2024-11-07 23:07:21 +0100

    Responsive changes

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,9 +4,8 @@ import Socials from "./Socials"
 
 export default function Header() {
   return (
-    <div class=" sticky top-0 flex max-h-screen w-1/2 flex-col py-32">
+    <div class=" md:sticky top-0 flex max-h-screen w-1/2 flex-col py-32">
       <Hero />
-      <Socials />
     </div>
   )
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -95,7 +95,7 @@ export default function Main() {
         class=" pt-24 h-auto scroll-mt-16 md:mb-24 lg:mb-4 lg:scroll-mt-12"
       >
         <div class="sticky top-0 z-20 -mx-6 mb-4 w-screen px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
-          <h2 class="text-sm font-bold uppercase tracking-widest text-textPrimary lg:sr-only">
+          <h2 class="text-xl font-bold uppercase tracking-widest text-textPrimary lg:sr-only">
             About
           </h2>
         </div>
@@ -127,8 +127,8 @@ export default function Main() {
         id="education"
         className="!mb-60 scroll-mt-6 md:mb-24 lg:mb-36 lg:scroll-mt-24"
       >
-        <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
-          <h2 class="text-sm font-bold uppercase tracking-widest text-textPrimary lg:sr-only">
+        <div class="sticky top-0 z-20 -mx-6 mb-4 w-screen px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
+          <h2 class="text-xl font-bold uppercase tracking-widest text-textPrimary lg:sr-only">
             Education
           </h2>
         </div>
@@ -170,8 +170,8 @@ export default function Main() {
         id="projects"
         className="mb-16 mt-32 scroll-mt-16 md:mb-24 lg:mb-36 lg:scroll-mt-24"
       >
-        <div className="sticky top-0 z-20 -mx-6 mb-4 w-screen bg-slate-900/75 px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
-          <h2 className="text-sm font-bold uppercase tracking-widest text-textPrimary lg:sr-only">
+        <div class="sticky top-0 z-20 -mx-6 mb-4 w-screen px-6 py-5 backdrop-blur md:-mx-12 md:px-12 lg:sr-only lg:relative lg:top-auto lg:mx-auto lg:w-full lg:px-0 lg:py-0 lg:opacity-0">
+          <h2 class="text-xl font-bold uppercase tracking-widest text-textPrimary lg:sr-only">
             Projects
           </h2>
         </div>
